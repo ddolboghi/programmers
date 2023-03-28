5c1ffbdc9d53599bbe3660558b198f343c5eca95
suj_zzzi <104489678+thstnwl@users.noreply.github.com>
2023-03-28 13:01:50 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,11 +1,20 @@
 def solution(lottos, win_nums):
-    # 0이 모두 당첨 번호 --> 최고 순위
-    # 0이 모두 당첨 번호 아니면 --> 최저 순위
-    # 최저 순위 = 불일치 번호 개수 + 1, 불일치 5개 이상부터는 6
-    # 최고 순위 = 불일치 번호 개수 + 1 - 0 개수
-    dif = len(set(win_nums) - set(lottos))
-    min_rank = dif + 1 if dif <5 else 6
-    zero = len([x for x in lottos if x == 0])
-    max_rank = dif if zero==0 and dif==6 else dif - zero + 1
-    print(set(lottos)&set(win_nums))
-    return [max_rank, min_rank]
+    answer = [] #최고순위, 최저순위
+    low = 1
+    zero = lottos.count(0)
+        
+    # 틀린갯수 + 1 = 등수 : 틀린갯수 5개 이상이면 그냥 6등
+    # 최저 ) 틀린갯수 + 1 : 0도 다 틀린거라고 생각
+    for i in lottos :
+        if i not in win_nums :
+            low += 1
+    if low >= 6 :
+        low = 6
+        
+    # 최고 ) 틀린갯수 + 1 - 0갯수 : 0을 다 맞춘거라고 생각
+    # 만약 틀린갯수 + 1 - 0갯수 가 0이하면 그냥 1등
+    if low - zero <= 0 :
+        zero = low - 1
+        
+    answer = [low-zero, low]
+    return answer
