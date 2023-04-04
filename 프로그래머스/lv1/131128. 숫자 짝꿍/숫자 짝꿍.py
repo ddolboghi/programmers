069bffacf82de396a63aae85ca952a865f50b01a
suj_zzzi <104489678+thstnwl@users.noreply.github.com>
2023-04-04 12:08:10 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,26 +1,26 @@
 def solution(X, Y):
-    # 1. X와 Y의 각 자리수별로 나타나는 숫자를 카운트합니다.
-    X_count = [0] * 10
-    Y_count = [0] * 10
+    answer = ''
+    # 각 자리수별로 숫자 카운트(0~9)
+    X_count, Y_count = [0] * 10, [0] * 10
+
     for x in X:
         X_count[int(x)] += 1
     for y in Y:
         Y_count[int(y)] += 1
     
-    # 2. X와 Y에서 공통으로 나타나는 숫자들을 찾습니다.
-    common_digits = set(str(X)) & set(str(Y))
+    # 공통인 숫자
+    common = set(str(X)) and set(str(Y))
     
-    # 3. 각 공통된 숫자에 대해, X와 Y에서 해당 숫자가 나타나는 개수 중 작은 값을 선택합니다.
-    counts = [min(X_count[int(d)], Y_count[int(d)]) for d in common_digits]
+    # 공통된 숫자에 대해, X와 Y에서 해당 숫자가 나타나는 개수 중 작은 값
+    counts = [min(X_count[int(d)], Y_count[int(d)]) for d in common]
     
-    # 4. 선택한 개수만큼 숫자를 이어붙여서 가장 큰 수를 만듭니다.
-    result = ''
-    for d, c in sorted(zip(common_digits, counts), reverse=True):
-        result += d * c
+    # 선택한 개수만큼 숫자 이어붙이기
+    for d, c in sorted(zip(common, counts), reverse=True):
+        answer += d * c
     
-    if result == '':
+    if answer == '':
         return '-1'
-    elif set(result) == {'0'}:
+    elif set(answer) == {'0'}:
         return '0'
     else:
-        return result
+        return answer
