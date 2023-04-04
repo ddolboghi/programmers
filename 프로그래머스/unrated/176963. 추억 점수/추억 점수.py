752805d4d81dfe2cf5b01807502075e0a634cc5d
ddolboghi <100576111+ddolboghi@users.noreply.github.com>
2023-04-04 15:29:58 +0900

    [unrated] Title: 추억 점수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
@@ -1,11 +1,12 @@
 def solution(name, yearning, photo):
-    answer = []
-    d = dict(zip(name, yearning))
-    print(d)
-    for onep in photo:
-        inp = set(name) & set(onep)
-        temp = 0
-        for i in inp:
-            temp += d[i]
-        answer.append(temp)
-    return answer
+    # answer = []
+    # d = dict(zip(name, yearning))
+    # print(d)
+    # for onep in photo:
+    #     inp = set(name) & set(onep)
+    #     temp = 0
+    #     for i in inp:
+    #         temp += d[i]
+    #     answer.append(temp)
+    # return answer
+    return [sum(yearning[name.index(j)] for j in i if j in name) for i in photo]
