262fb9910449dce85724a8221abba1b187110812
suj_zzzi <104489678+thstnwl@users.noreply.github.com>
2023-04-04 10:54:29 +0900

    [unrated] Title: 덧칠하기, Time: 23.42 ms, Memory: 12.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
@@ -0,0 +1,8 @@
+def solution(n, m, section):
+    section.sort()
+    answer = 0
+    i = 0
+    # m으로 나눠서 몫만 (몫이 같으면 한번에 칠할 수 있음/ 첫번째수가 시작이므로)
+    a = [5, 1, 2]
+    print(a.sort())
+    return answer
