9f31196bd301a08dbe7bc2988366804be271aa29
UiJoon64 <117344692+UiJoon64@users.noreply.github.com>
2023-05-19 19:14:34 +0900

    Delete 프로그래머스/unrated directory

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
+++
@@ -1,9 +0,0 @@
-def solution(cards1, cards2, goal):
-    # cards1, cards2각각 순서대로 가면서 goal 원소와 같은지 비교
-    # cards의 첫번째 원소부터 같아야함 --> 같으면 첫번째 원소 제거
-    # 길이 > 0 --> No
-    for g in goal:
-        if len(cards1) and g == cards1[0]: cards1.pop(0)
-        elif len(cards2) and g == cards2[0]: cards2.pop(0)
-        else: return 'No' 
-    return 'Yes'
