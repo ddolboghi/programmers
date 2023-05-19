b2d0b63be5907b94821b506b7fad6f6523e55181
UiJoon64 <117344692+UiJoon64@users.noreply.github.com>
2023-05-19 19:18:37 +0900

    Delete 프로그래머스/unrated directory

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++
@@ -1,2 +0,0 @@
-def solution(k, score):
-    return [sorted(score[:i])[0] if i<=k else sorted(score[:i], reverse=True)[:k][-1] for i in range(1, len(score)+1) ]
--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
+++
@@ -1,8 +0,0 @@
-def solution(t, p):
-    answer = 0
-    for i in range(0,len(t)-len(p)+1,1):
-        number = int(t[i:i+len(p)])
-        if number <= int(p) :
-            answer += 1
-        
-    return answer
