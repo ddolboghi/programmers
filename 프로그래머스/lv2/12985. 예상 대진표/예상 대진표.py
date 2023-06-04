9514e6952141c0f68e5a2f760955d868b97cdacb
ddolboghi <100576111+ddolboghi@users.noreply.github.com>
2023-06-04 23:38:31 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
@@ -0,0 +1,9 @@
+def solution(n,a,b):
+    answer = 0
+    i = 1
+    while n >= 2**i:
+        if (a-1)//(2**i) == (b-1)//(2**i):
+            answer = i
+            break
+        i += 1
+    return answer
