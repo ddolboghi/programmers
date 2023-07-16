c9e30f5393a4ddd2894f6c904c935e0860909b4b
ddolboghi <100576111+ddolboghi@users.noreply.github.com>
2023-07-16 16:11:58 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -19,21 +19,29 @@
 #         answer.append(cnt)
 #         i += cnt
 #     return answer
+
+# def solution2(progresses, speeds):
+#     answer = []
+#     time = 0
+#     count = 0
+#     while len(progresses)> 0:
+#         if (progresses[0] + time*speeds[0]) >= 100:
+#             progresses.pop(0)
+#             speeds.pop(0)
+#             count += 1
+#         else:
+#             if count > 0:
+#                 answer.append(count)
+#                 count = 0
+#             time += 1
+#     answer.append(count)
+#     return answer
+
 def solution(progresses, speeds):
-    print(progresses)
-    print(speeds)
-    answer = []
-    time = 0
-    count = 0
-    while len(progresses)> 0:
-        if (progresses[0] + time*speeds[0]) >= 100:
-            progresses.pop(0)
-            speeds.pop(0)
-            count += 1
+    Q=[]
+    for p, s in zip(progresses, speeds):
+        if len(Q)==0 or Q[-1][0]<-((p-100)//s):
+            Q.append([-((p-100)//s),1])
         else:
-            if count > 0:
-                answer.append(count)
-                count = 0
-            time += 1
-    answer.append(count)
-    return answer
+            Q[-1][1]+=1
+    return [q[1] for q in Q]
