df5eaae2b0f0fef515b1d0a120a7ef5699ec9833
ddolboghi <100576111+ddolboghi@users.noreply.github.com>
2023-07-30 15:33:41 +0900

    Update 공주님의 정원.py

--- 백준/Gold/2457. 공주님의 정원/공주님의 정원.py
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.py
@@ -4,54 +4,37 @@ import sys
 n = int(sys.stdin.readline())
 
 # 꽃들이 피고 지는 날짜
-arr = []
+dates = []
 for _ in range(n):
     # 꽃이 피고 지는 날짜에 대해 월에 100을 곱한 뒤, 일이랑 덧셈
     start_m, start_d, end_m, end_d = map(int, sys.stdin.readline().split())
-    arr.append([start_m * 100 + start_d, end_m * 100 + end_d])
+    dates.append([start_m * 100 + start_d, end_m * 100 + end_d])
+dates.sort() # 오름차순 정렬
 
-# 꽃이 피는 날짜, 꽃이 지는 날짜순으로 오름차순 정렬
-arr.sort()
-
-# 정원의 마지막 꽃이 지는 날짜
-end_date = 301
-
-# 심은 꽃의 개수
-count = 0
-
-# 더 이상 확인할 꽃이 없을때까지
-while (arr):
-
-    # 정원의 마지막 꽃이 지는 날짜가 12월 1일 이상이 됐거나,
-    # 현재 확인할 꽃의 시작 날짜가 정원의 마지막 꽃이 지는 날짜와 이어지지 않을 경우, 탐색 종료
-    if (end_date >= 1201 or arr[0][0] > end_date):
+end = 301
+cnt = 0
+while dates:
+    # 첫 시작날짜가 301보다 크면 0 출력해야함
+    # 저번 반복의 마지막 끝날짜 > 1130면 cnt만 보면됌 
+    #  끝날짜 < 첫 시작날짜면 (앞조건에서 걸렀으므로)비는 날이 생기는 경우만 발생
+    if end >= 1201 or dates[0][0] > end:
         break
-
-    # 꽃이 피는 날짜가 end_date 이전일 때, 가장 느리게 지는 꽃의 날짜
-    temp_end_date = -1
-
-    for _ in range(len(arr)):
-
-        # 꽃이 피는 날짜가 end_date 이전이라면,
-        if (arr[0][0] <= end_date):
-            # 그 중 가장 느리게 지는 꽃의 날짜를 확인
-            if (temp_end_date <= arr[0][1]):
-                temp_end_date = arr[0][1]
-
-            # 확인한 꽃은 원본 배열에서 제거
-            arr.remove(arr[0])
-
+    
+    # end보다 일찍 피는 꽃들 중에서 가장 늦게 지는 꽃 찾기
+    temp_end = -1
+    for _ in range(len(dates)):
+        if dates[0][0] <= end:
+            if temp_end <= dates[0][1]:
+                temp_end = dates[0][1] # 이전 end보다 크거나 같은 것만 저장
+            
+            dates.remove(dates[0])
         else:
             break
-
-    # 가장 꽃이 느리게 지는 날짜를 end_date로 수정
-    end_date = temp_end_date
-    # 심은 꽃의 개수 증가
-    count += 1
-
-# 마지막으로 확인한 꽃의 지는 날짜가 12월 1일 보다 작으면, 
-# 3월 1일부터 11월 30일까지 계속 피어있는게 아니므로 0 출력
-if end_date < 1201:
+    
+    end = temp_end
+    cnt += 1
+            
+if end < 1201:
     print(0)
 else:
-    print(count)
+    print(cnt)
