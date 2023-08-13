ee515e673048c952ade104241102e2f245254e98
ddolboghi <100576111+ddolboghi@users.noreply.github.com>
2023-08-13 19:04:14 +0900

    [Gold IV] Title: 빙산, Time: 2556 ms, Memory: 34940 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,34 +1,34 @@
 import sys
 from collections import deque
-input = sys.stdin.readline
+n, m = map(int, sys.stdin.readline().split())
+graph = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 
-
-def bfs(x, y):
-    q = deque([(x, y)])
-    visited[x][y] = 1
+def bfs(y, x):
+    q = deque([(y, x)])
+    check[y][x] = True
     seaList = []
     while q:
-        x, y = q.popleft()
+        py, px = q.popleft()
         sea = 0
+        #인접한 0개수 세기
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m:
-                if not graph[nx][ny]:
+            yy = py + dy[i]
+            xx = px + dx[i]
+            if 0<=yy<n and 0<=xx<m:
+                if not graph[yy][xx]:
                     sea += 1
-                elif graph[nx][ny] and not visited[nx][ny]:
-                    q.append((nx, ny))
-                    visited[nx][ny] = 1
-        if sea > 0:
-            seaList.append((x, y, sea))
-    for x, y, sea in seaList:
-        graph[x][y] = max(0, graph[x][y] - sea)
-
+                #빙산 방문
+                elif graph[yy][xx] and not check[yy][xx]:
+                    check[yy][xx] = True
+                    q.append((yy, xx))
+        if sea>0:
+            seaList.append((py, px, sea))
+    for y, x, sea in seaList:
+        graph[y][x] = max(0, graph[y][x]-sea)
     return 1
 
-
-n, m = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 
 ice = []
 for i in range(n):
@@ -36,24 +36,21 @@ for i in range(n):
         if graph[i][j]:
             ice.append((i, j))
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
 year = 0
-
 while ice:
-    visited = [[0] * m for _ in range(n)]
+    berg = 0
+    check = [[False]*m for _ in range(n)]
     delList = []
-    group = 0
-    for i, j in ice:
-        if graph[i][j] and not visited[i][j]:
-            group += bfs(i, j)
-        if graph[i][j] == 0:
-            delList.append((i, j))
-    if group > 1:
+    for y, x in ice:
+        if graph[y][x] and not check[y][x]:
+            berg += bfs(y, x)
+        if graph[y][x] == 0:
+            delList.append((y, x))
+    if berg>1:
         print(year)
         break
+    
     ice = sorted(list(set(ice) - set(delList)))
     year += 1
-
-if group < 2:
+if berg<2:
     print(0)
