a23097b926d99ddef86c82be1570252dcbe83d73
ddolboghi <100576111+ddolboghi@users.noreply.github.com>
2024-03-26 01:52:17 +0900

    [Gold V] Title: 로봇 청소기, Time: 76 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,68 +1,49 @@
 from collections import deque
+
 n, m = map(int, input().split())
 r, c, d = map(int, input().split())
 room = [list(map(int, input().split())) for _ in range(n)]
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
-def change(y, x, d):
-    if d==0:
-        d = 3
-        x -= 1
-    elif d==1:
-        d = 0
-        y -= 1
-    elif d==2:
-        d = 1
-        x += 1
-    else:
-        d = 2
-        y += 1
-    return y, x, d
-    
-def back(y, x, d):
-    if d==0:
-        y += 1
-    elif d==1:
-        x -= 1
-    elif d==2:
-        y -= 1
-    else:
-        x += 1
-    return y, x
-    
-def bfs(y, x, d):
-    q = deque([(y, x, d)])
-    room[y][x] = 2
-    clean = 1
-    while q:
-        py, px, pd = q.popleft()
-        empty = 0
-        for i in range(4):
-            yy = py + dy[i]
-            xx = px + dx[i]
-            if room[yy][xx] == 0:
-                empty += 1
-                
-        #인접 0있으면 0나올때까지 방향전환
-        if empty>0:
-            while True:
-                cy, cx, pd = change(py, px, pd)
-                if room[cy][cx] == 0:
-                    room[cy][cx] = 2
-                    q.append((cy, cx, pd))
-                    clean += 1
-                    break
-                    
-        
-        #인접 0없으면 후진
-        else:
-            by, bx = back(py, px, pd)
-            if 0<=by<n and 0<=bx<m:
-                if room[by][bx]==1:
-                    break
-                else:
-                    q.append((by, bx, pd))
-    return clean
+D = [3, 0, 1, 2] # 서 북 동 남, D[d] = 반시계 90도 방향
+backD = [2, 3, 0, 1] # 남 서 북 동 backD[d] = 후진 방향
+dxy = ((0, -1), (1, 0), (0, 1), (-1, 0)) # 북 동 남 서, dxy[d'] 방향으로 이동
 
-print(bfs(r, c, d))
+start = (c, r, d) # x, y, 방향
+q = deque([start])
+room[r][c] = 2
+cnt = 1
+while q:
+  x, y, dd = q.popleft()
+
+  # 청소 안한 곳 개수 세기
+  unclean = 0
+  for dx, dy in dxy:
+    xx = x + dx
+    yy = y + dy
+
+    if room[yy][xx] == 0:
+      unclean += 1
+
+  # 청소할 곳 없으면 후진
+  if unclean > 0:
+    while True:
+      dd = D[dd]
+      gdx, gdy = x+dxy[dd][0], y+dxy[dd][1]
+      if room[gdy][gdx] == 0:
+        room[gdy][gdx] = 2
+        q.append((gdx, gdy, dd))
+        cnt += 1
+        break
+  # 청소할 곳 있으면 청소할 좌표 나올때까지 방향 회전하기
+  else:
+    bdx, bdy = dxy[backD[dd]]
+    back_x = x + bdx
+    back_y = y + bdy
+    if 0<=back_x<m and 0<=back_y<n:
+      if room[back_y][back_x] == 1:
+        break
+      else:
+        q.append((back_x, back_y, dd))
+    
+
+print(cnt)
