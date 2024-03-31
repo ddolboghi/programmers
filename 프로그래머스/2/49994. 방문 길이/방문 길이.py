0bd9c2583bf25ff3dba1af803ccc38cd4894e4ba
ddolboghi <100576111+ddolboghi@users.noreply.github.com>
2024-04-01 01:10:17 +0900

    [level 2] Title: 방문 길이, Time: 2.35 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,24 +1,14 @@
 def solution(dirs):
-    start = (0, 0)
-    length = 1
     history = []
-    for i in range(len(dirs)):
-        end = move(dirs[i], start)
+    dxy = {"U":(0, 1), "D": (0, -1), "R": (1, 0), "L": (-1, 0)}
+    start = (0, 0)
+    for d in dirs:
+        xx = start[0] + dxy[d][0]
+        yy = start[1] + dxy[d][1]
+        x = xx if -5 <= xx <= 5 else start[0]
+        y = yy if -5 <= yy <= 5 else start[1]
+        end = (x, y)
         if (start, end) not in history and (end, start) not in history and start != end:
             history.append((start, end))
-        
         start = end
-        
-    print(len(history), history)
-    answer = 0
     return len(history)
-
-def move(dir, pos):
-    dx = {"U":0, "D":0, "R":1, "L":-1}
-    dy = {"U":1, "D":-1, "R":0, "L":0}
-    #pos[0]: x, pos[1]: y
-    goX = pos[0] + dx[dir]
-    goY = pos[1] + dy[dir]
-    x = goX if -5 <= goX <= 5 else pos[0]
-    y = goY if -5 <= goY <= 5 else pos[1]
-    return (x, y)
