f465656bdf62f05e0133ff575c3bfd593389a587
ddolboghi <100576111+ddolboghi@users.noreply.github.com>
2024-04-02 00:41:33 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.py
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.py
@@ -1,18 +1,17 @@
 def solution(n):
-    # 1. n(몫)을 3으로 나누어서 나머지 == 0이면 4로 바꾸고 answer++ 아니면 나머지 바로 answer++
-    # 1-1. 나머지 == 0일때 몫--
-    # 2. 몫 < 3이 될때까지 나누고 1번 반복
-    # 3. 마지막 몫 answer++        
+    # 3보다 작아질때까지 나누기
+    # 나머지가 0이면 4로 바꾸고 다음 차례에 (몫-1)/3
     answer = ''
-    
     while n >= 3:
         mod = n % 3
         n //= 3
         if mod == 0:
-            answer += '4'
+            answer += "4"
             n -= 1
         else:
             answer += str(mod)
-            
-    answer += str(n%3) if n != 0 else ''
+
+    # 마지막 몫 합치기
+    answer += str(n) if n != 0 else ""
+    
     return answer[::-1]
