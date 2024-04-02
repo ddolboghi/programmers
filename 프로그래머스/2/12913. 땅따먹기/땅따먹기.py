6579ecaca8c3297196d92564d1e152e3695af333
ddolboghi <100576111+ddolboghi@users.noreply.github.com>
2024-04-03 00:23:00 +0900

    [level 2] Title: 땅따먹기, Time: 444.51 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,6 +1,6 @@
-def solution(land):
+def solution(land):    
     for i in range(1, len(land)):
-        for j in range(4):
-            land[i][j] += max([land[i-1][k] for k in range(4) if k != j])
-    
+        for j in range(len(land[i])):
+            land[i][j] = max([land[i][j] + land[i-1][k] for k in range(len(land[i-1])) if j != k])
+
     return max(land[-1])
