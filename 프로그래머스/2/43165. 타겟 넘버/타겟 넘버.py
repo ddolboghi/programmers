67d42df9954e3d44c2eeca10aa9b988cacf7524f
ddolboghi <100576111+ddolboghi@users.noreply.github.com>
2024-04-04 00:47:37 +0900

    [level 2] Title: 타겟 넘버, Time: 4.83 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,18 +1,18 @@
+from collections import deque
+def solution(numbers, target):
+    return bfs(numbers[0], target, numbers) + bfs(-numbers[0], target, numbers)
 
-def solution(numbers, target):     
-    return dfs(target, numbers, 0, 0, 0)
-
-def dfs(target, numbers, idx, sum, result):
-    
-    if idx >= len(numbers):
-        if target == sum:
-            result += 1
-        return result
-    
-    return dfs(target, numbers, idx+1, sum + numbers[idx], result) + dfs(target, numbers, idx+1, sum - numbers[idx], result)
-    
-    
-    
-    
-            
+def bfs(start, target, numbers):
+    count = 0
+    q = deque([(start, 0)]) # (숫자, 계산 횟수)
+    while q:
+        n, cnt = q.popleft()
         
+        if cnt == len(numbers)-1:
+            if n == target:
+                count += 1
+            continue
+        
+        q.append((n + numbers[cnt+1], cnt+1))
+        q.append((n - numbers[cnt+1], cnt+1))
+    return count
