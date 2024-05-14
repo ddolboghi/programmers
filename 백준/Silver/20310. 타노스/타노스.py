e45708c21ff995bb99b39fc5c5f7fe110bdc8a73
ddolboghi <100576111+ddolboghi@users.noreply.github.com>
2024-05-14 18:31:49 +0900

    [Silver III] Title: 타노스, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -1,4 +1,11 @@
-s = input()
-zero = s.count("0") // 2
-one = s.count("1") // 2
-print("0"*zero+"1"*one)
+arr = list(input())
+
+zero, one = arr.count('0')//2, arr.count('1')//2
+
+for _ in range(zero):
+  arr.pop(-arr[::-1].index('0')-1)
+
+for _ in range(one):
+  arr.pop(arr.index('1'))
+
+print(''.join(arr))
